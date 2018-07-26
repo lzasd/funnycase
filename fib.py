eab3e26f7668415293a7c376e2b376892819e3cb
lzbr <lzbr@qq.com>
2018-07-26 22:05:27 +0800

    优化递归

--- fib.py
+++ fib.py
@@ -6,15 +6,20 @@ def fib1():
         yield a
         a = a+b
         a,b = b,a
-
-# 递归
+import functools
+import time
+# 递归 使用lru_cache缓存机制 优化递归
+@functools.lru_cache()
 def fib2(n):
     if n < 2:
         return 1 if n==2 else n
     return fib2(n-2)+ fib2(n-1)
 
-fib_list = list(map(fib2,range(10)))
-print(fib_list)
+t1 = time.time()
+print(fib2(500))
+t2 = time.time()
+
+print(t2-t1)
 
 
 
