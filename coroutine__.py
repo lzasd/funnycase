c8a559332b2b3396c76d244e9fddcd7ce0ba47a4
lzbr <3385008263@qq.com>
2018-08-07 11:18:54 +0800

    定义预激装饰器装饰协成

--- coroutine__.py
+++ coroutine__.py
@@ -1,5 +1,17 @@
 # 使用协程 动态的计算平均值
 
+
+# 使用预激装饰器进行优化
+import functools
+def coroutine(func):
+    @functools.wraps(func)
+    def wraper(*args,**kwargs):
+        g = func(*args,**kwargs)
+        next(g)
+        return g
+    return wraper
+
+@coroutine
 def averager():
 
     totle = 0.0
@@ -13,6 +25,6 @@ def averager():
 
 
 ave = averager()
-next(ave)
+
 print(ave.send(10))
 print(ave.send(20))
