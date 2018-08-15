457dc01a6bfc1e145f931528ffb48925eb2fd26e
lzbr <3385008263@qq.com>
2018-08-15 15:25:52 +0800

    补充快速排序的两种实现方法

--- stack__.py
+++ stack__.py
@@ -14,9 +14,9 @@ def	bubbleSort(alist):
         if point:
             return
 
-alist=[1,2,3,99,4]
-bubbleSort(alist)
-print(alist)
+# alist=[1,2,3,99,4]
+# bubbleSort(alist)
+# print(alist)
 
 
 # select_sort， 选择排序，遍历一次找出序列中最小的一个放在第一位，确定第一个位置，再遍历剩下的找出最小的放在第二个位子
@@ -68,7 +68,53 @@ def	insertionSort(alist):
 
 
 
-# 快速排序
+# 快速排序:首先任意选取一个数据（通常选用数组的第一个数）作为关键数据，
+# 然后将所有比它小的数都放到它前面，
+# 所有比它大的数都放到它后面，这个过程称为一趟快速排序。
+#O(nlgn) 不稳定
+def QuickSort(myList,start,end):
+    #判断low是否小于high,如果为false,直接返回
+    if start < end:
+        i,j = start,end
+        #设置基准数
+        base = myList[i]
+
+        while i < j:
+            #如果列表后边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while (i < j) and (myList[j] >= base):
+                j = j - 1
+
+            #如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            myList[i] = myList[j]
+
+            #同样的方式比较前半区
+            while (i < j) and (myList[i] <= base):
+                i = i + 1
+            myList[j] = myList[i]
+        #做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        myList[i] = base
+
+        #递归前后半区
+        QuickSort(myList, start, i - 1)
+        QuickSort(myList, j + 1, end)
+    return myList
+
+
+# myList = [49,38,65,97,76,13,27,49]
+# QuickSort(myList,0,len(myList)-1)
+# print(myList)
+
+# 匿名函数实现快拍
+
+#  选出第一个元素  把比他小的放在左边比他大的放在右边，
+# 得到的结果后把比他小的那一边和比他大的那一边分别进行这个操作
+
+quick_sort = lambda array: array if len(array) <= 1 else quick_sort(
+    [item for item in array[1:] if item < array[0]]) + [array[0]] + quick_sort(
+    [item for item in array[1:] if item >= array[0]])
+
+print(quick_sort([1,223,2,312312,332,332,45]))
+
 
 
 
--- tree__.py
+++ tree__.py
@@ -2,6 +2,7 @@
 
 
 class BinaryTree:
+
     def __init__(self, root):
         self.key = root
         self.left_child = None
